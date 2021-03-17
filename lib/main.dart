f818f6c7d1ccd2688c9a49c1dee3c633ba746102
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-03-17 11:18:35 +0700

    add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +9,43 @@ void main() {
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: BallState(),
+      ),
+    );
   }
 }
 
+class BallState extends StatefulWidget {
+  @override
+  _BallStateState createState() => _BallStateState();
+}
+
+class _BallStateState extends State<BallState> {
+
+  int randomImage = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Expanded(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                randomImage = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$randomImage.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
